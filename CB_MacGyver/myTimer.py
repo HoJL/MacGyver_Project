6efdf9768747f7fe5e11f6843a50c54468a9bf54
말랑곰 <leehoje000@gmail.com>
2024-03-25 01:00:03 +0900

    다운로드 스레드가 끝난뒤에도 타이머 쓰레드가 종료되지 않는 문제 해결

--- CB_MacGyver/myTimer.py
+++ CB_MacGyver/myTimer.py
@@ -6,6 +6,7 @@ class MyTimer():
 
         self.__interval = 1
         self.__slot = slot
+        self.isStop = False
 
     def setInterval(self, interval):
         self.__interval = interval
@@ -15,7 +16,7 @@ class MyTimer():
         self.timer.start()
 
     def stop(self):
-        self.timer.cancel()
+        self.isStop = True
 
     def connect(self, slot):
         self.__slot = slot
@@ -23,5 +24,8 @@ class MyTimer():
     def __timer(self):
         if self.__slot is not None:
             self.__slot()
+        if self.isStop is True:
+            self.timer.cancel()
+            return
         self.timer = threading.Timer(self.__interval, self.__timer)
         self.timer.start()
