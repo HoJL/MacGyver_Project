f1d5413da441270c95a1957a9e8e8fa4fbfcdbc2
말랑곰 <leehoje000@gmail.com>
2024-04-05 00:05:18 +0900

    번역추가

--- CB_MacGyver/qt5.pro
+++ CB_MacGyver/qt5.pro
@@ -1,4 +1,4 @@
-SOURCES = MainUI.py customWidget/custom_progress_bar.py customWidget/customEdit.py 
+SOURCES = MainUI.py customWidget/custom_progress_bar.py customWidget/customEdit.py customWidget/download_panel.py
 
 TRANSLATIONS = \
     qt5_ko_kr.ts \
--- CB_MacGyver/qt5_ko_kr.ts
+++ CB_MacGyver/qt5_ko_kr.ts
@@ -4,12 +4,12 @@
 <context>
     <name>CustomLineEdit</name>
     <message>
-        <location filename="customWidget/customEdit.py" line="28"/>
+        <location filename="customWidget/customEdit.py" line="27"/>
         <source>Enter URL</source>
         <translation>URL을 입력하세요</translation>
     </message>
     <message>
-        <location filename="customWidget/customEdit.py" line="104"/>
+        <location filename="customWidget/customEdit.py" line="89"/>
         <source>Paste</source>
         <translation>붙여넣기</translation>
     </message>
@@ -17,20 +17,63 @@
 <context>
     <name>CustomProgressBar</name>
     <message>
-        <location filename="customWidget/custom_progress_bar.py" line="116"/>
+        <location filename="customWidget/custom_progress_bar.py" line="120"/>
         <source>processing...</source>
         <translation>후 처리중...</translation>
     </message>
 </context>
 <context>
+    <name>Download_Panel</name>
+    <message>
+        <location filename="customWidget/download_panel.py" line="57"/>
+        <source>Forder Open</source>
+        <translation>폴더 열기</translation>
+    </message>
+    <message>
+        <location filename="customWidget/download_panel.py" line="61"/>
+        <source>Remove from list</source>
+        <translation>목록에서 제거</translation>
+    </message>
+    <message>
+        <location filename="customWidget/download_panel.py" line="232"/>
+        <source>Are you sure you want to delete this task from the list?</source>
+        <translation>이 작업을 목록에서 제거하시겠습니까?</translation>
+    </message>
+    <message>
+        <location filename="customWidget/download_panel.py" line="233"/>
+        <source>Delete List</source>
+        <translation>목록에서 제거</translation>
+    </message>
+    <message>
+        <location filename="customWidget/download_panel.py" line="238"/>
+        <source>Are you sure you want to delete this file?</source>
+        <translation>이 파일을 삭제하시겠습니까?</translation>
+    </message>
+    <message>
+        <location filename="customWidget/download_panel.py" line="239"/>
+        <source>Delete File</source>
+        <translation>파일 삭제</translation>
+    </message>
+    <message>
+        <location filename="customWidget/download_panel.py" line="251"/>
+        <source>&amp;Yes</source>
+        <translation>예</translation>
+    </message>
+    <message>
+        <location filename="customWidget/download_panel.py" line="252"/>
+        <source>&amp;No</source>
+        <translation>아니요</translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
-        <location filename="MainUI.py" line="123"/>
+        <location filename="MainUI.py" line="130"/>
         <source>Exit</source>
         <translation>나가기</translation>
     </message>
     <message>
-        <location filename="MainUI.py" line="124"/>
+        <location filename="MainUI.py" line="131"/>
         <source>Tasks</source>
         <translation>작업</translation>
     </message>
